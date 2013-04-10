dab53bc664af9f8b98d512ac21908d2b3f2dbf5a
J Clayton Tallieu <joseph_tallieu@dell.com>
2013-04-10 14:47:50 -0500

    Added the Dummy transport

--- wsman/transport/dummy/__init__.py
+++ wsman/transport/dummy/__init__.py
@@ -1,5 +1,5 @@
 """
-Transport for WS-Management
+Dummy transport 
 
 @copyright: 2010-2012
 @author: Joseph Tallieu <joseph_tallieu@dell.com>
@@ -23,21 +23,20 @@ Transport for WS-Management
 #    along with WSManAPI.  If not, see <http://www.gnu.org/licenses/>.
 
 
-class Transport(object):
+import os
+import sys
+import time
+import random
+import logging
+
+log = logging.getLogger("WSMAN.transport")
+from .. import Transport
+
+
+class Dummy(Transport):
     """
-    The Transport class is responsible for executing the
-    constructed command.    
+    Dummy transport
     """
-    
-    def __init__(self, **kwargs):
-        """
-        Constructor for the transport
-        """
-        
-        # controls if the transport should write anything to the console
-        self.__quiet = False
-        pass
-    
     
     def execute(self, command):
         """
@@ -49,21 +48,27 @@ class Transport(object):
         @return: The output from the command execution 
         @rtype: String
         """
+        start = time.time()
         
-        raise NotImplementedError("This method needs to be implemented by the derived class.")
-    
-    def __set_quiet_mode(self, value):
-        """
-        Set verbosity of the transport
-        """
+        # You could parse the command line here and send the appropriate file
+        # but the parser needs to handle all of these formats. This transport
+        # picks the file randomly. 
         
-        if isinstance(value, bool):
-            self.__quiet = value
-        else:
-            pass
+        files = ['wsmanfault.txt', 'epr.txt', 'get.txt', 'fault.txt', 'instance.txt', 'instances.txt', 'invoke_resp.txt', 'invoke_result.txt']                
+        #files = ['invoke_resp.txt', 'invoke_result.txt']
+        #files = ['get.txt', 'instance.txt', 'instances.txt', 'wsmanfault.txt']
+        #files = ['fault.txt']
+        files = ['large_response.txt']
         
-    
-    quiet = property(fget=lambda x: x.__quiet, fset=__set_quiet_mode)    
+        filename = random.choice(files)
+        basename = os.path.dirname(__file__)
+        #provider = 'winrm' if (sys.platform == 'win32') else 'wsmancli'
+        provider = 'winrm'
+        path = os.path.join(basename, 'responses', provider, filename)
+        print path
         
-    
+        duration = time.time() - start
+        output = open(path, "r").read()
+        log.info("Command Completed in %0.3f s" % duration, extra={'command': command, 'output': output, 'duration':duration})
+        return output
     
